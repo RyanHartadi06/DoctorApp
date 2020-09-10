b8a62b13431bd66d26e7be00e04c5c7770d0b982
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-09-11 01:46:18 +0700

    upload

---
+++ src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBdcITPMzGA1Le9h13i54ZnlGhejoApQKY',
+  authDomain: 'my-doctor-01-203b0.firebaseapp.com',
+  databaseURL: 'https://my-doctor-01-203b0.firebaseio.com',
+  projectId: 'my-doctor-01-203b0',
+  storageBucket: 'my-doctor-01-203b0.appspot.com',
+  messagingSenderId: '1087414056636',
+  appId: '1:1087414056636:web:e0a0ebbf17145867fa42b9',
+});
+
+const Fire = firebase;
+
+export default Fire;
